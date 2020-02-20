48ccf188d443d1aae1a893bbe0ceda68bdc57222
Timur Abdymazhinov <abdymazhinov.timur@gmail.com>
2020-02-20 20:24:41 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let buttonStart  = document.getElementById('start'),
+    resultTable = document.querySelector('.result-table'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesItemButton = document.getElementsByClassName('expenses-item-btn'),
+    countBudgetBtn = document.querySelector('.count-budget-btn'),
+    optionalExpenses = document.getElementsByClassName('optionalexpenses');
